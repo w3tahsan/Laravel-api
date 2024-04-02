c61a87d107d9a32c00149f52f4583940a2505b8c
tahsan2 <96606113+tahsan2@users.noreply.github.com>
2024-04-02 11:28:08 +0600

    third

--- app/Http/Controllers/API/ProductApiController.php
+++ app/Http/Controllers/API/ProductApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Inventory;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
@@ -14,9 +15,20 @@ class ProductApiController extends Controller
         return response()->json($products);
     }
     function get_product_details($slug){
+        $product_id = Product::where('slug', $slug)->first()->id;
         $product_info = Product::where('slug', $slug)->with('rel_to_cat')->with('rel_to_brand')->get();
+        $available_colors = Inventory::where('product_id', $product_id)
+            ->groupBy('color_id')
+            ->selectRaw('sum(color_id) as sum, color_id')
+            ->with('rel_to_color')->get();
+        $available_sizes = Inventory::where('product_id', $product_id)
+            ->groupBy('size_id')
+            ->selectRaw('sum(size_id) as sum, size_id')
+            ->with('rel_to_size')->get();
         $response = [
             'product_info' => $product_info,
+            'available_colors' => $available_colors,
+            'available_sizes' => $available_sizes,
             'product_img_link' => 'uploads/product/preview/'.$product_info->first()->preview,
         ];
 
